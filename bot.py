b98eaf67f6d6b9286eeaab036fffebe25f9cf2f6
MOHIT5145 <uvrohit004@gmail.com>
2025-06-30 23:00:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ from Script import script
 from datetime import date, datetime 
 import pytz
 from aiohttp import web
-
+from Zahid.server import web_server
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
